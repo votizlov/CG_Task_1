d896af6a9af007dbbbeeca5b0b27ce3562462199
Votizlov <seriousfox@yandex.ru>
2019-09-17 12:19:43 +0300

    Initial commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,14 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.ArcType;
+
+public class Controller {
+    @FXML
+    public GridPane gridPane;
+
+}
